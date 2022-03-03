175cb51f6c710fef50b82e1e70c812e5a46eb928
unknown <davidye0201@gmail.com>
2022-03-02 21:05:00 -0500

    Created the map function

--- methods.js
+++ methods.js
@@ -9,12 +9,45 @@ Array.prototype.myEach = function (callbackFn) {
         callbackFn(this[i], i, this);   // callbackFn is "console.log(x,i,myArray)" 
     }
 };
+// TEST //
+// Test myEach against the native forEach to ensure that myEach works as the same as forEach
+let myArray = [1,2,,4,5];   // Array called by the function
+
+// Test with 1 parameter: element
+console.log("myEach (1 parameter): element");
+myArray.myEach(x => console.log(x));   // Parameter: x => console.log(x)
+console.log("forEach (1 parameter): element");
+myArray.forEach(x => console.log(x));   // Parameter: x => console.log(x)
+
+// Test with 2 parameters: element, index
+console.log("myEach (2 parameters): element, index");
+myArray.myEach((x,i) => console.log(x,i));   // Parameter: (x,i) => console.log(x,i)
+console.log("forEach (2 parameters): element, index");
+myArray.forEach((x,i) => console.log(x,i));   // Parameter: (x,i) => console.log(x,i)
+
+// Test with 3 parameters: element, index, array
+console.log("myEach (3 parameters): element, index, array");
+myArray.myEach((x,i,myArray) => console.log(x,i,myArray));   // Parameter: (x,i,myArray) => console.log(x,i,myArray)
+console.log("forEach (3 parameters): element, index, array");
+myArray.forEach((x,i,myArray) => console.log(x,i,myArray));   // Parameter: (x,i,myArray) => console.log(x,i,myArray)
+
+// Original array is not affected
+console.log("Original array:");
+console.log(myArray);
 
 // MAP //
-Array.prototype.myMap = function() {
-    
+Array.prototype.myMap = function(callbackFn) {
+    //Create a new array.
+    mapArray = []
+    for (let i = 0; i < this.length; i++) {
+        if (this[i] === undefined) continue;
+        //Creating the new values of the array to push into the new array
+        const result = callbackFn(this[i], i, this)
+        mapArray.push(result)
+    }
+    //Returning the array
+    return mapArray;
 };
-
 /** 
 // SOME //
 Array.prototype.mySome = function() {
